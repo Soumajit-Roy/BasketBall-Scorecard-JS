2e93a1b9482d7938c0eba9f5ddc9063c33f5ea32
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-07-20 00:09:34 +0530

    First day

---
+++ index.js
@@ -0,0 +1,30 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+//Home Score functions
+let count = 0
+function score1(){
+    count += 1
+    homeScore.textContent = count
+}
+function score2(){
+    count += 2
+    homeScore.textContent = count
+}
+function score3(){
+    count += 3
+    homeScore.textContent = count
+}
+//Guest score functions
+let gcount = 0
+function gscore1(){
+    gcount += 1
+    guestScore.textContent = gcount
+}
+function gscore2(){
+    gcount += 2
+    guestScore.textContent = gcount
+}
+function gscore3(){
+    gcount += 3
+    guestScore.textContent = gcount
+}
